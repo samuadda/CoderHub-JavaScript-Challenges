9254e9911a36f318964c4caf6e0ea97ee3d4f1f6
saddiq musa <112342835+samuadda@users.noreply.github.com>
2023-11-01 21:26:04 +0300

    Update 1#satr_102_oop.js

--- 1#satr_102_oop.js
+++ 1#satr_102_oop.js
@@ -1,3 +1,27 @@
+//OOP Paradigm Programming 
+
+/*Transportation company project
+Project Idea
+A transportation company wants to assign its cars and planes to a number of drivers or pilots. Therefore, the idea of the application is to help them assign the vehicle, whether it is a car or a plane, to a qualified driver or pilot, so that planes must be assigned to a pilot and cars must be assigned to a driver.
+
+##Steps
+> Create a class for vehicles and define their properties such as (name, manufacturer, ID).
+> Create a Class for the car so that it inherits from the Vehicles Class and define the characteristics of the car (type of car, gas or electric...).
+> Create a Class for the Airplane so that it inherits from the Vehicles Class and define the characteristics of the plane (type of plane...).
+> Create a Class with the name employee to represent employees and define its properties such as (name, ID, date of birth).
+> Create a Driver Class that inherits from the Employee Class with additional driver properties defined such as licenseID.
+> Create a Pilot class that inherits from the Employee class and defines additional pilot properties such as licenseID.
+> Create a Reservation Class that contains the driver and his vehicle, such as (reservationDate, employeeId, vehiclesId, reservationID).
+> After completing the above creation, define objects for three cars and two planes.
+> Write a function that receives the employee's ID and the vehicle's ID and checks. If the employee is a pilot and the vehicle is a car, a message that they are not compatible will be printed. If the employee is a pilot and the vehicle is an airplane, an object will be created and stored in the Class reserved.
+> Save all reservations into an array.
+> Print the content of the array using the map function.
+
+##By the end of this project the student will be able to:
+Dealing with object-oriented programming (OOP) and its properties in JavaScript.
+Dealing with matrix functions.
+Dealing with functions in more depth.*/
+
 // Created class for vehicles
 class Vehicles {
     constructor(name ,manifactorar, identity){
