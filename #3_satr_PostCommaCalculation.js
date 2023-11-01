17a3816715a5bbddd70aa48af59ee610a745e67a
saddiq musa <112342835+samuadda@users.noreply.github.com>
2023-11-01 21:31:53 +0300

    Update #3_satr_PostCommaCalculation.js

--- #3_satr_PostCommaCalculation.js
+++ #3_satr_PostCommaCalculation.js
@@ -1,14 +1,26 @@
-function Decimal_places(num) {
-    // regexp to match numbers after the comma
-    let regexp = /\.\d+/;
+//Post comma calculation
 
-    // converts the numbers to string
+/* Write a function that receives a string value that is a decimal or integer.
+The function counts the number of digits after the comma and returns the result as an integer value.*/
+
+function Decimal_places(num) {
+    // Convert the number to a string to work with regular expressions
     let str = num.toString();
 
-    // check if the numbers match the exp
+    // Define a regular expression pattern to match decimal places
+    let regexp = /\.\d+/;
+
+    // Check if the regular expression pattern is found in the string
     if (str.match(regexp)) {
-        return str.match(regexp)[0].length - 1; // then counting the result(matching) -1 which is the comma
+        // If found, return the length of the matched decimal part (excluding the decimal point)
+        return str.match(regexp)[0].length - 1;
     } else {
+        // If no decimal places found, return 0
         return 0;
     }
 }
+
+// Example usage:
+const number = 12.3456;
+const result = Decimal_places(number);
+console.log("Number of decimal places: " + result);
