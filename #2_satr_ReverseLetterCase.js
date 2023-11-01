cc6f2bcfc67ced58a357713a48343226721ae602
saddiq musa <112342835+samuadda@users.noreply.github.com>
2023-11-01 21:35:56 +0300

    Update #2_satr_ReverseLetterCase.js

--- #2_satr_ReverseLetterCase.js
+++ #2_satr_ReverseLetterCase.js
@@ -1,15 +1,30 @@
-function reverse_case(strParam) {
-    // converts the string to array
-    let letter = strParam.split("");
+//Reverse letter case
 
-    //loop over all letters
-    for (let i = 0; i < letter.length; i++) {
-        //checking if the letter is upperCase or lowerCase then converts it
-        if (letter[i] === letter[i].toUpperCase()) {
-            letter[i] = letter[i].toLowerCase();
+/*Write a function that receives a value of type string. 
+The function changes the case of letters by converting a lowercase letter to an uppercase letter and vice versa, 
+and then returns a value of type string.*/
+
+function reverse_case(strParam) {
+    // Converts the input string to an array of individual letters
+    let letters = strParam.split("");
+
+    // Loop over all the letters in the array
+    for (let i = 0; i < letters.length; i++) {
+        // Check if the letter is uppercase or lowercase, then convert it to the opposite case
+        if (letters[i] === letters[i].toUpperCase()) {
+            letters[i] = letters[i].toLowerCase();
         } else {
-            letter[i] = letter[i].toUpperCase();
+            letters[i] = letters[i].toUpperCase();
         }
     }
-    return letter.join(""); // reJoin the array to a string and returns it
+
+    // Rejoin the array of letters to form a string and return the result
+    return letters.join("");
 }
+
+// Example usage:
+const inputString = "Hello, World!";
+const result = reverse_case(inputString);
+console.log("Reversed Case: " + result);
+
+
