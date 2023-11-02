e477fe49b178f29961a26f6f4d4a8e74055c2d7c
saddiq musa <saddiq0musa@gmail.com>
2023-11-02 20:09:41 +0300

    uploded the first file

---
+++ 1_Rectangle_area.js
@@ -0,0 +1,7 @@
+function area_of_rectangle(width, height) {
+    // Calculate and return the area of the rectangle, which is the product of width and height.
+    return width * height;
+}
+
+// Example usage:
+console.log(area_of_rectangle(5, 4));
