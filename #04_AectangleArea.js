a39ac0d6cc2387eef5842349d1aeecc0fc902d34
saddiq musa <saddiq0musa@gmail.com>
2023-11-02 19:03:47 +0300

    updated names of the files

---
+++ #04_AectangleArea.js
@@ -0,0 +1,10 @@
+//rectangle area
+
+function area_of_rectangle(width, height) {
+    // Calculate and return the area of the rectangle, which is the product of width and height.
+    return width * height;
+}
+
+// Example usage:
+const rectangleArea = area_of_rectangle(5, 4);
+console.log("Rectangle Area: " + rectangleArea);
